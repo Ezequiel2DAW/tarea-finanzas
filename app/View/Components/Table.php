1ac953e916733beaf1652224a9c98ade44f596fb
Ezequiel Barroso <ebargon775@g.educaand.com>
2025-01-22 14:40:04 +0100

    Cambiando la tabla a un componenete

--- app/View/Components/Table.php
+++ app/View/Components/Table.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\View\Components\Layouts;
+namespace App\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Index extends Component
+class Table extends Component
 {
     /**
      * Create a new component instance.
      */
-    public function __construct(public String $title)
+    public function __construct()
     {
         //
     }
@@ -21,6 +21,6 @@ class Index extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layouts.index');
+        return view('components.table');
     }
 }
