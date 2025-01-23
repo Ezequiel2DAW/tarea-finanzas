af0b5aaf6d3102103569cc7c7268af9842e91836
Ezequiel2DAW <ebargon775@g.educaand.com>
2025-01-23 14:28:59 +0100

    Botón genérico agregado v.2

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,7 +4,7 @@
         <a href="{{$data[1]}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{$slot}}</a>
         @break
     @case('name')
-        <input type="submit" name="{{$data[1]}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{$slot}}</input>
+        <input type="submit" name="{{$data[1]}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
         @break
 @endswitch
 
